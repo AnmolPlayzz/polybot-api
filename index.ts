0fea7d5838abc45722180b85d4b170fb1e6635f7
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2024-07-26 14:51:27 +0530

    v0.1 - server info

---
+++ index.ts
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+dotenv.config();
+const PORT: number = Number(process.env.PORT) || 3000;
+const TOKEN: string | undefined = process.env.TOKEN;
+if (!TOKEN) {
+    throw new Error("No token found!")
+}
+import express, {Express, Request, Response} from "express";
+import {Client, GatewayIntentBits, Guild, Snowflake} from "discord.js";
+
+const client: Client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+const app: Express = express();
+app.use(express.json());
+
+app.get("/server",async (req: Request, res: Response) => {
+    const { id }: {
+        id: Snowflake | undefined
+    } = req.body;
+    if (!id) return res.status(403).json({
+        error: "No ID found!"
+    })
+    try {
+        const server: Guild = await client.guilds.fetch(id)
+        const memberCount: number = server.memberCount;
+        const botCount: number = (await server.members.fetch()).filter(m => m.user.bot).size
+        const roleCount: number = (await server.roles.fetch()).size
+        const channelCount: number = (await server.channels.fetch()).size
+        res.status(200).json({
+            id: id,
+            memberCount,
+            botCount,
+            roleCount,
+            channelCount,
+        })
+    } catch (e: any) {
+        console.error(e)
+        res.status(500).json({
+            error: e.message,
+        })
+    }
+})
+
+client.on("ready", async () => {
+    console.log("Discord.JS client ready!")
+})
+
+client.login(TOKEN)
+
+app.listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+    console.log(`Address: http://localhost:${PORT}`)
+}).on("error", (error) => {
+    throw new Error(error.message);
+});
